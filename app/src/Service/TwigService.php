df06d02800cbb7c137d5686069d37e0df30a5c84
apiterov <arcxe@icloud.com>
2025-04-10 02:21:46 +0300

    Интерфейс для Twig

--- app/src/Service/TwigService.php
+++ app/src/Service/TwigService.php
@@ -2,29 +2,20 @@
 
 namespace App\Service;
 
+use App\Contract\TwigServiceInterface;
 use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
 use Twig\Loader\FilesystemLoader;
 
-final class TwigService
+class TwigService implements TwigServiceInterface
 {
-    private static ?self $instance = null;
-    private Environment $twig;
-
-    private function __construct()
-    {
+    private function __construct(
+        private Environment $twig
+    ) {
         $loader = new FilesystemLoader('views');
         $this->twig = new Environment($loader);
-    }
-
-    public static function init(): self
-    {
-        if (self::$instance === null) {
-            self::$instance = new self();
-        }
-        return self::$instance;
     }
 
     /**
