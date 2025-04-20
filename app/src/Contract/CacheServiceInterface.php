074f8da1485576d79eb37a88bdff054967c7cfef
apiterov <arcxe@icloud.com>
2025-04-21 02:07:42 +0300

    Добавил тесты, поправил ошибку в интерфейсе кеша

--- app/src/Contract/CacheServiceInterface.php
+++ app/src/Contract/CacheServiceInterface.php
@@ -6,7 +6,7 @@ interface CacheServiceInterface
 {
     public function get(string $key): mixed;
 
-    public function set(string $key, mixed $value, int $expiration): bool;
+    public function set(string $key, mixed $value, int $expiration = 0): bool;
 
     public function delete(string $key): bool;
 }
---
+++ app/tests/CacheServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Contract\CacheServiceInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class CacheServiceTest extends TestCase
+{
+    private MockObject $cacheServiceMock;
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    protected function setUp(): void
+    {
+        $this->cacheServiceMock = $this->createMock(CacheServiceInterface::class);
+    }
+
+    public function testSet(): void
+    {
+        $this->cacheServiceMock->method('set')->willReturn(true);
+        $result = $this->cacheServiceMock->set('key', 'value');
+        $this->assertTrue($result);
+    }
+
+    public function testGet(): void
+    {
+        $this->cacheServiceMock->method('get')->willReturn('value');
+        $result = $this->cacheServiceMock->get('key');
+        $this->assertEquals('value', $result);
+    }
+
+    public function testDelete(): void
+    {
+        $this->cacheServiceMock->method('delete')->willReturn(true);
+        $result = $this->cacheServiceMock->delete('key');
+        $this->assertTrue($result);
+    }
+}
